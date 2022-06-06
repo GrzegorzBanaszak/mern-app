1ade29a0f79520bc82ddcf0f1f60af1b7fab1bad
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-06 18:06:14 +0200

    Create simple server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.yellow.bold);
+});
