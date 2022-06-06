d7c00838fb0884be77e77298a26a9e115c23a674
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-06 18:23:16 +0200

    Create router

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use("/api/goals", require("./routes/goalRoutes"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`.yellow.bold);
 });
