880a50dba60d3d9467fa83d0120b6fe4797fc3b8
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-06 21:19:01 +0200

    Mongo schema

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const morgan = require("morgan");
 const errorHandler = require("./middleware/errorMiddleware");
+const connectDb = require("./config/db");
 const PORT = process.env.PORT || 5000;
+
+connectDb();
+
 const app = express();
 
 app.use(express.json());
